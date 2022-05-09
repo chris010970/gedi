79d542461ad350cefac196a665a144c655a28d3e
chris010970 <c.r.williams0109@gmail.com>
2022-05-09 13:33:43 +0100

    Initial revision of L4A data parser

---
+++ l4a/src/gedil4a.py
@@ -0,0 +1,293 @@
+import os
+import glob
+import h5py
+import requests
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+from datetime import datetime
+from shapely.ops import orient
+from shapely.geometry import Polygon
+from shapely.geometry import MultiPolygon
+
+from sqlalchemy import create_engine
+
+
+class GediL4a():
+
+    # constants
+    doi = '10.3334/ORNLDAAC/2056'
+    base_url = 'https://cmr.earthdata.nasa.gov/search/'
+    page_size = 2000
+
+
+    def __init__( self, pathname ):
+
+        # open file as read-only
+        self._hf = h5py.File( pathname, 'r' )
+
+        # base datetime to convert l4a delta times
+        self._base_time = datetime(2018, 1, 1, 0, 0, 0)
+        return
+
+
+    def getBeamData( self, aoi=None ):
+
+
+
+        # empty dataframe
+        df = pd.DataFrame()
+
+        # scan through keys
+        for key in list( self._hf.keys()):
+            if key.startswith( 'BEAM'):
+
+                # get beam group
+                group = self._hf.get( key )
+                beam = self.getGroupData( group )
+
+                # reject null retrievals
+                beam = beam.replace( -9999, np.nan )    
+                beam = beam[ beam[ 'agbd' ].notna() ]
+
+                # apply basic qa filtering
+                beam = beam[ beam[ 'algorithm_run_flag' ] > 0 ]
+                beam = beam[( beam[ 'l2_quality_flag' ] == 1) & ( beam[ 'l4_quality_flag' ] == 1 ) ]
+
+                # create datetime column
+                beam[ 'datetime' ] = self._base_time + pd.to_timedelta( beam.delta_time, unit='s' )
+
+                # drop superfluous columns
+                beam = beam.drop( [ 'algorithm_run_flag', 'l2_quality_flag', 'l4_quality_flag' ], axis=1 )
+                df = pd.concat( [ x for x in [ df, beam ] if not x.empty ], ignore_index=True )
+
+        # convert to a geopandas dataframe
+        gdf = gpd.GeoDataFrame( df, geometry=gpd.points_from_xy( df.lon_lowestmode, df.lat_lowestmode ) )
+        gdf = gdf.drop( [ 'lon_lowestmode', 'lat_lowestmode' ], axis=1 )
+        gdf = gdf.set_crs( 'EPSG:4326' )
+
+        # filter on aoi
+        if aoi is not None:
+            gdf = gdf[ gdf['geometry'].within( aoi ) ]
+
+        return gdf
+
+
+    def getGroupData( self, group ):
+        
+        # iterate through values
+        names, values = [], []
+        for key, value in group.items():
+                    
+            if not isinstance( value, h5py.Group ):
+                
+                # 1d vars
+                if ( len(value.shape) == 1 ):
+                    names.append( key )
+                    values.append( value[:].tolist() )
+                else:
+                    # handling for 2d covariance matrices
+                    if ( len(value.shape) == 2 ):
+                        for idx in range( value.shape[1] ):
+                            names.append( key + '_' + str( idx + 1 ) )
+                            values.append( value[:, idx].tolist() )
+                    else:
+                        # ignore 3d params for now
+                        continue
+
+        return pd.DataFrame( map(list, zip(*values)), columns=names )
+
+
+    def getGeolocationData( self, aoi=None ):
+
+        # intermediate lists
+        lat_l, lon_l, time_l, beam_n, shot_l = [], [], [], [], []
+
+        # scan through keys
+        for key in list( self._hf.keys()):
+            if key.startswith('BEAM'):
+
+                # get beam group
+                beam = self._hf.get( key )
+
+                # retrieve coords and times
+                lat = beam.get('lat_lowestmode')[:]
+                lon = beam.get('lon_lowestmode')[:]
+                time = beam.get('delta_time')[:] 
+                shot = beam.get('shot_number')[:] 
+
+                # add to current list
+                lat_l.extend(lat.tolist())
+                lon_l.extend(lon.tolist())
+                time_l.extend(time.tolist()) 
+                shot_l.extend(shot.tolist()) 
+
+                # number of shots in the beam group
+                n = lat.shape[0] 
+                beam_n.extend( np.repeat( str(key), n).tolist() )
+            
+        # create dataframe with datetime column
+        df = pd.DataFrame( list(zip(shot_l,beam_n,lat_l,lon_l,time_l) ), columns=['shot_number', 'beam', 'lat', 'lon', 'delta_time' ] )
+        df[ 'datetime' ] = self._base_time + pd.to_timedelta( df.delta_time, unit='s' )
+
+        # convert to geodataframe registered to geographic crs
+        gdf = gpd.GeoDataFrame(  df, geometry=gpd.points_from_xy( df.lon, df.lat ) )
+        gdf.set_crs( 'EPSG:4326' )
+
+        # turn fill values (-9999) to nan
+        gdf = gdf.drop( [ 'lat', 'lon', 'delta_time' ], axis=1 )
+        gdf = gdf.replace( -9999, np.nan )    
+
+        # filter on aoi
+        if aoi is not None:
+            gdf = gdf[ gdf['geometry'].within( aoi ) ]
+
+        return gdf
+
+
+    @staticmethod
+    def getGranuleMetadata( aoi ):
+
+        def getCollectionConceptId():
+
+            # retrieve metadata catalogue
+            doisearch = GediL4a.base_url + f'collections.json?doi={GediL4a.doi}'
+            return requests.get(doisearch).json()['feed']['entry'][0]['id']
+
+        # initialise vars
+        metadata = list()    
+        page_num = 1
+
+        # loop until break
+        while True:
+        
+            # define search and payload parameters
+            data = {
+                "collection_concept_id": getCollectionConceptId(), 
+                "page_size": GediL4a.page_size,
+                "page_num": page_num,
+                "simplify-shapefile": 'true' # required to bypass 5000 coordinates limit of CMR
+            }
+
+            payload = { "shapefile": ( "search.json", aoi.geometry.to_json(), "application/geo+json" ) }
+
+            # execute post search        
+            search = GediL4a.base_url + 'granules.json'
+            response = requests.post( search, data=data, files=payload)
+            granules = response.json()['feed']['entry']
+        
+            # granules in response ?
+            if granules:
+                
+                # iterate through granules
+                for g in granules:
+                                    
+                    # capture info in dictionary
+                    info = { 'url' : '', 'geometry' : '', 'size' : float( g['granule_size'] ) }
+                    if 'polygons' in g:
+
+                        # split multifeature list into separate objects
+                        polygons = []
+                        for polygon in g['polygons']:
+                            i=iter(polygon[0].split(" "))
+
+                            # append to list
+                            ltln = list(map(" ".join,zip(i,i)))
+                            polygons.append(Polygon([[float(p.split(" ")[1]), float(p.split(" ")[0])] for p in ltln]))
+
+                        # combine polygons into single object
+                        info[ 'geometry' ] = MultiPolygon( polygons )
+
+                    # get dataset url
+                    for links in g['links']:
+                        if 'title' in links and links['title'].startswith('Download') and links['title'].endswith('.h5'):
+                            info[ 'url' ] = links['href']
+
+                    # add to list        
+                    metadata.append( info )
+                
+                # next page
+                page_num += 1
+            
+            else: 
+                # end of granule search
+                break
+            
+        # construct metadata geodataframe registered to geographic crs
+        df = gpd.GeoDataFrame( metadata )
+        df = df.set_crs( "EPSG:4326" )
+        return df.drop_duplicates( subset=['url'] )
+
+
+    def getKenyaAoi( path, names ):
+
+        # load selected county boundaries
+        def getCounties( names ):         
+            return admin[ admin [ 'Name' ].isin( names ) ]
+
+        # load kenyan admin boundaries
+        admin = gpd.read_file( os.path.join( path, 'admin.shp' ) ) 
+        counties = getCounties( names )
+
+        # expand before dissolve to ensure boundaries overlap
+        counties['geometry'] = counties['geometry'].buffer(0.001)
+        counties = counties.dissolve( aggfunc='sum')
+
+        # orient polygon points clockwise
+        counties.geometry = counties.geometry.apply( orient, args=(1,) )
+        return counties
+
+
+    def runTests():
+
+        # get area of interest dataframe
+        county_names = ['Kiambu', 'Laikipia', 'Nakuru', 'Nyandarua', 'Nyeri' ]
+        aoi = GediL4a.getKenyaAoi( os.path.join( root_path, 'aois/kenya' ), county_names )
+
+        # grab meta record of datasets collocated with aoi
+        #metadata = GediL4a.getGranuleMetadata( aoi )            
+        #print ( 'meta records: {}'.format( len( metadata ) ) )
+            
+        # get list of available datasets
+        files = glob.glob( '{path}\\*.h5'.format( path=os.path.join( root_path, 'l4a/data/kenya' ) ) ) 
+        obj = GediL4a( files[ 0 ] )
+        
+        # create geolocation dataframe
+        #geoloc = obj.getGeolocationData()
+        #print( geoloc )
+
+        gdf = obj.getBeamData( aoi=aoi.geometry.iloc[ 0 ] )
+        gdf = gdf.set_index( 'shot_number' )
+
+        # Set up database connection engine
+        engine = create_engine('postgresql://test:&VxEos00@34.163.194.102:5432/4rd-climate-finance')
+
+        """
+        with engine.connect() as connection:
+            result = connection.execute( ("select datetime from gedil4a.kenya"))
+            for row in result:
+                print( row['datetime'] )
+        """
+
+        # GeoDataFrame to PostGIS
+        gdf.to_postgis( con=engine,
+                        name="kenya",
+                        schema="gedil4a",
+                        if_exists='append', 
+                        index=True )
+
+        return
+
+
+# execute main
+if __name__ == '__main__':
+
+    # get repo root path
+    repo = 'gedi'
+    root_path = os.getcwd()[ 0 : os.getcwd().find( repo ) + len ( repo )]
+
+    GediL4a.runTests()
+
+
+
